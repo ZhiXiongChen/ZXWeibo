cc940a6c7a2357e3eb092c7762b10e75116165f0
ZhiXiongChen <784105464@qq.com>
2017-07-23 14:58:29 +0800

    Initial Commit

---
+++ ZXWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZXWeibo
+//
+//  Created by zhixiongchen on 2017/7/23.
+//  Copyright © 2017年 zhixiongchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
